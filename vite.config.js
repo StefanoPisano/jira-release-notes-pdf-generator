cb580a4b3af5241f524afc138eb83cb3bc392cd1
Stefano Pisano <spisano@keywordsstudios.com>
2026-05-14 14:44:55 +0200

    Update deploy to use gh-pages branch

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3333'
+      '/api': 'http://127.0.0.1:3333'
     }
   }
 })
